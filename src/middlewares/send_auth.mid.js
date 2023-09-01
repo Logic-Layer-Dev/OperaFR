2d5380530ed9ce499b59c7f84b09d4b7e3676d35
Carlos Giongo <carlosgiongoo@gmail.com>
2023-09-01 14:26:13 -0300

    [BUFGIX] Wrong return of 401 and refactor new middleware

--- src/middlewares/send_auth.mid.js
+++ src/middlewares/send_auth.mid.js
@@ -1,10 +1,9 @@
+const defaultResponse = require("../utils/defaultResponse");
+
 require("dotenv").config();
 
 module.exports = async (req, res, next) => {
     const valid_ips = JSON.parse(process.env.VALID_SENDER_IPS);
-
-    if(valid_ips.includes('*')) next();
-    if(!valid_ips.includes(req.ip)) return res.status(401).send({error: 'Unauthorized'});
-
+    if(!valid_ips.includes(req.ip) && !valid_ips.includes('*')) return res.status(401).json(defaultResponse(401, `Unauthorized`, null))
     next();
 }

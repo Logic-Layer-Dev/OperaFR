b69d3c624c443b9a0685e0a43a57b5df8c0ecab4
Carlos Giongo <carlosgiongoo@gmail.com>
2024-09-23 14:32:34 -0300

    feat:finished basic dashboard
    Finished basic dashboard for file upload analysis

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,7 +1,17 @@
+require('dotenv').config(); 
 const path = require('path');
 
 module.exports = function(app) {
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, '../views'));
+
     app.get('/dashboard', (req, res) => {
-        res.sendFile(path.join(__dirname, '../dashboard/index.html')); 
+        const serverIp = process.env.SERVER_IP; 
+        res.render('dashboard', { serverIp }); 
+    });
+
+    app.get('/dashboard/login', (req, res) => {
+        const serverIp = process.env.SERVER_IP; 
+        res.render('login', { serverIp }); 
     });
 }
